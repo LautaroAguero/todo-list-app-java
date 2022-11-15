344e455fd404884432679cb36b56b8eeefac3285
wlko <cristiangconci@gmail.com>
2022-11-15 18:59:13 -0300

    Comenzando funcionalidades de tareas

--- src/AppFrame.java
+++ src/AppFrame.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AppFrame extends JFrame {
+
     private JButton addTask;
     private JButton clearTask;
 
     TitleBar title = new TitleBar();
     ButtonPanel buttonPanel = new ButtonPanel();
+    List list = new List();
 
     public AppFrame () {
         this.setSize(400,700);
@@ -14,6 +16,7 @@ public class AppFrame extends JFrame {
         this.setVisible(true);
         this.add(this.title, BorderLayout.NORTH);
         this.add(this.buttonPanel,BorderLayout.SOUTH);
+        this.add(this.list,BorderLayout.CENTER);
 
         addTask = buttonPanel.getAddTaskButton();
         clearTask = buttonPanel.getClearTaskButton();
